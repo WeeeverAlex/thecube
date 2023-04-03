3fd5b65d5acffa8ea74f8f031269bf1ebaaf744c
WeeverAlex <awever4000@gmail.com>
2023-04-03 17:06:35 -0300

    montando estrutura cubo

--- cubo.py
+++ cubo.py
@@ -2,39 +2,60 @@ import pygame
 from pygame.locals import *
 import numpy as np
 
-#initialization
+#inicalização
 pygame.init()
 pygame.font.init()
 
-#screen
+#tela
 screen_width = 720
-screen_height= 00
+screen_height= 600
 screen = pygame.display.set_mode((screen_width , screen_height))
 pygame.display.set_caption("the cube")
 
 #FPS
 clock = pygame.time.Clock()
-FPS = 60  # Frames per Second
+FPS = 60  # Frames pOr Second
+
+# Distância focal
+d = 400
+
+# Cor do cubo
+color = (0,0 , 100)
+
+# Criando a matriz que representa o cubo em três dimensões
+cubo = np.array([[-100, -100, -100, 1], [100, -100, -100, 1], [100, 100, -100, 1], [-100, 100, -100, 1], [-100, -100, 100, 1], [100, -100, 100, 1], [100, 100, 100, 1], [-100, 100, 100, 1]]).T
+
+# Matriz pinhole
+pinhole = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,-d],[0,0,-(1/d),0]])
+
+# Ângulo de rotação
+angulo = np.deg2rad(1)
+
+# Matriz de rotação em X
+x = np.array([[1, 0, 0, 0], [0, np.cos(angulo), -np.sin(angulo), 0], [0, np.sin(angulo), np.cos(angulo), 0], [0, 0, 0, 1]])
+
+# Matriz de rotação em Y
+y = np.array([[np.cos(angulo), 0, np.sin(angulo), 0], [0, 1, 0, 0], [-np.sin(angulo), 0, np.cos(angulo), 0], [0, 0, 0, 1]])
+
+# Matriz de rotação em Z
+z = np.array([[np.cos(angulo), -np.sin(angulo), 0, 0], [np.sin(angulo), np.cos(angulo), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+# Matriz de rotação total
+r = x @ y @ z
+
+# Matrizes de translação em Z
+Tz = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, d], [0, 0, 0, 1]])
+
+# Matriz de translação para o centro da tela
+Tc = np.array([[1, 0, 0, 400], [0, 1, 0, 300], [0, 0, 1, 0], [0, 0, 0, 1]])
 
 
-game = True
-#----------------------------GAME LOGIC----------------------------#
-
-while game:
-
-
-    # Handle events
+# Loop principal
+main = True
+while main:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game = False
+            main = False
 
-    # Update the display
-    pygame.display.update()
 
-    screen.fill((0, 0, 0))
-
-    # Limit the frame rate
-    clock.tick(60)
-
-# Quit Pygame
 pygame.quit()
