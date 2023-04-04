170e8c99aa08666c1f94cb5b095973cd41cc2f99
WeeverAlex <awever4000@gmail.com>
2023-04-04 15:51:44 -0300

    commit

--- cubo.py
+++ cubo.py
@@ -1,4 +1,40 @@
 import pygame
 from pygame.locals import *
+import numpy as np
+
+#initialization
+pygame.init()
+pygame.font.init()
+
+#screen
+screen_width = 720
+screen_height= 00
+screen = pygame.display.set_mode((screen_width , screen_height))
+pygame.display.set_caption("the cube")
+
+#FPS
+clock = pygame.time.Clock()
+FPS = 60  # Frames per Second
 
 
+game = True
+#----------------------------GAME LOGIC----------------------------#
+
+while game:
+
+
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    # Update the display
+    pygame.display.update()
+
+    screen.fill((0, 0, 0))
+
+    # Limit the frame rate
+    clock.tick(60)
+
+# Quit Pygame
+pygame.quit()
